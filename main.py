4044f18014ff0cc15dd2e6772ba136975268b8e8
Esmira Melikzade <esmira.malikzada.std@bhos.edu.az>
2026-07-16 22:22:28 +0400

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel
 app = FastAPI()
 class TaskCreate(BaseModel):
     title: str
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
 tasks = [
     {
         "id": 1,
@@ -47,3 +50,29 @@ def create_task(task: TaskCreate):
     }
     tasks.append(new_task)
     return new_task
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskUpdate):
+    if not updated_task.title.strip():
+        raise HTTPException(
+            status_code=400,
+            detail={"error": "Title is required"}
+        )
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["done"] = updated_task.done
+            return task
+    raise HTTPException(
+        status_code=404,
+        detail={"error": f"Task {task_id} not found"}
+    )
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail={"error": f"Task {task_id} not found"}
+    )
