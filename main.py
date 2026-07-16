f81a57a98cb9e5e71194d0d1ca3c390750b45b4c
Esmira Melikzade <esmira.malikzada.std@bhos.edu.az>
2026-07-16 20:20:01 +0400

    Stage 2: read endpoints with tasks

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello World"}
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Build my first API",
+        "done": True
+    },
+    {
+        "id": 3,
+        "title": "Upload project to GitHub",
+        "done": False
+    }
+]
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail={"error": f"Task {task_id} not found"}
+    )
